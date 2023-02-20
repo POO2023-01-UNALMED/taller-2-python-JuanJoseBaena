03a8cdf445e631e8ab6a807ac5892d55a87d8591
JuanJoseBaena <124928411+JuanJoseBaena@users.noreply.github.com>
2023-02-20 08:50:52 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+class Auto:
+    def __innit__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        self.cantidadCreados = cantidadCreados
+
+        def cantidadAsientos(self, cantidadAsientos):
+            numeroAsientos = 0
+            for asiento in self.asientos:
+                if isinstance(asiento, asientos):
+                    numeroAsientos += 1
+            return(cantidadAsientos)
+
+        def verificarIdentidad(self,):
+            if self.motor != self.registro:
+                return "Las piezas no son originales"
+            elif self.asientos != self.registro:
+                return "Las piezas no son originales"
+            else:
+                return "Auto original"
+            return verificarIdentidad
+
+class Asientos:
+    def __innit__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+        def cambiarColor(self, colores):
+            colores = ["rojo", "verde", "amarillo", "negro", "blanco"]
+            if color in colores:
+                self.color = color
+            return cambiarColor
+
+class Motor:
+    def __innit__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+        def cambiarRegistro (self, registro):
+            registroNuevo = int(input())
+            if registroNuevo != registro:
+                self.registro = registroNuevo
+            return cambiarRegistro
+
+        def asignarTipo (self, tipo):
+            self.tipo = tipo
+            if tipo == "electrico" or "combustible":
+                asignarTipo
