156db0333d3bef422d26db86638143ce83ef0efc
JuanJoseBaena <124928411+JuanJoseBaena@users.noreply.github.com>
2023-02-21 08:00:37 -0500

    Version 4.5

--- main.py
+++ main.py
@@ -9,22 +9,31 @@ class Auto:
         self.registro = registro
 
         def cantidadAsientos(self):
-            numeroAsientos = 0
-
-            for asiento in self.asientos:
-                if type (asiento)==Asiento:
-                    numeroAsientos += 1
-            return numeroAsientos
+            count=0
+            for i in self.asientos:
+                if i !=None:
+                    count +=1
+            return count
+            #numeroAsientos = 0
+            #for asiento in self.asientos:
+                #if type (asiento)==Asiento:
+                    #numeroAsientos += 1
+            #return numeroAsientos
 
         def verificarIntegridad(self):
             if (self.registro==self.motor.registro):
-                for asiento in self.asientos:
-                    if type(asiento)==Asiento:
-                        if asiento.registro !=self.registro:
-                            return "Las piezas no son originales"
-                return "Auto original"
-            else:
                 return "Las piezas no son originales"
+            for i in range(len(self.asientos)):
+                if self.asientos[i] !=None and self.asientos[i].registro != self.registro:
+                    return "Las piezas no son originales"
+            return "Auto original"
+                #for asiento in self.asientos:
+                    #if type(asiento)==Asiento:
+                        #if asiento.registro !=self.registro:
+                            #return "Las piezas no son originales"
+                #return "Auto original"
+            #else:
+                #return "Las piezas no son originales"
 
 
             #for asiento in self.asientos:
@@ -44,9 +53,11 @@ class Asiento:
         self.registro = registro
 
     def cambiarColor(self, color):
-        if (color=="rojo" or color=="verde" or color=="amarillo" or color=="negro" or color=="blanco"):
-            self.color=color
-        #colores = ["rojo", "verde", "amarillo", "negro", "blanco"]
+        #if (color=="rojo" or color=="verde" or color=="amarillo" or color=="negro" or color=="blanco"):
+            #self.color=color
+        colores = ["rojo", "verde", "amarillo", "negro", "blanco"]
+        if color.lower() in colores:
+            self.color = color
         #if color in colores:
             #self.color = color
 
@@ -60,10 +71,10 @@ class Motor:
             self.registro = registro
 
         def asignarTipo (self, tipo):
-            if (tipo=="electrico" or tipo=="gasolina"):
-                self.tipo=tipo
-            #tipos = ["electrico", "gasolina"]
-            #if tipo in tipos:
-                #self.tipos = tipo
+            #if (tipo=="electrico" or tipo=="gasolina"):
+                #self.tipo=tipo
+            tipos = ["electrico", "gasolina"]
+            if tipo.lower() in tipos:
+                self.tipos = tipo
 
-#Version 4.0
+#Version 4.5
